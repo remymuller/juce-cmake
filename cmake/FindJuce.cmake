c717bc3c3e9aac170dc6d18ca64555f4210ffe49
Remy Muller <r.muller@uvi.net>
2017-08-29 16:04:18 +0200

    added missing AudioUnit dependency added juce_add_audio_plugin

--- cmake/FindJuce.cmake
+++ cmake/FindJuce.cmake
@@ -312,6 +312,14 @@ macro(juce_add_module module)
 
 		# generate immutable INTERFACE IMPORTED library for module
         if(NOT TARGET ${module})
+
+            # force missing dependencies
+            if(${module} MATCHES juce_audio_plugin_client)
+                if(APPLE)
+                    list(APPEND JUCE_${module}_platformlibs ${JUCE_LIB_AudioUnit})
+                endif()
+            endif()
+
     		add_library(${module} INTERFACE IMPORTED)
             set_property(TARGET ${module} PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${JUCE_${module}_searchpaths})
             set_property(TARGET ${module} PROPERTY INTERFACE_LINK_LIBRARIES 
@@ -337,6 +345,12 @@ macro(juce_add_module module)
 		endforeach()
 	endif()
 endmacro()
+
+# hard coded add_dependencies
+if(APPLE)
+    find_library(JUCE_LIB_AudioUnit AudioUnit)
+    mark_as_advanced(JUCE_LIB_AudioUnit)
+endif()
 
 #------------------------------------------------------------------------------
 # First find JUCE
@@ -444,6 +458,10 @@ foreach(module ${JUCE_MODULES})
             if("${JUCE_MODULES_PREFIX}/${module}/${module_source_basename}.mm" IN_LIST _module_FILES)
                 set(_ext "mm")
             endif()
+
+            if("${JUCE_MODULES_PREFIX}/${module}/${module_source_basename}.r" IN_LIST _module_FILES)
+                set(_ext "r")
+            endif()
         endif()
 
         if(NOT DEFINED _ext)
@@ -509,3 +527,308 @@ find_package_handle_standard_args(JUCE DEFAULT_MSG
     JUCE_ROOT_DIR
 	JUCE_INCLUDE_DIR 
 )
+
+
+# export some helper functions
+
+# first remember where we are
+set(JUCE_CMAKE_MODULE_DIR ${CMAKE_CURRENT_LIST_DIR} CACHE INTERNAL "")
+
+function(juce_add_vst target sources)
+    set(OSX_EXTENSION "vst")
+    set(OSX_INSTALL_PATH "$(HOME)/Library/Audio/Plug-Ins/VST/")
+    set(VST_PLIST_IN "${JUCE_CMAKE_MODULE_DIR}/FindJuceTemplates/Info-VST.plist.in")
+    set(VST_PLIST "${CMAKE_BINARY_DIR}/JuceLibraryCode/${target}_Info.plist")
+
+    configure_file("${VST_PLIST_IN}" "${VST_PLIST}" @ONLY)
+
+    add_library(${target} MODULE ${sources})
+    set_target_properties(${target} 
+        PROPERTIES 
+            OUTPUT_NAME ${PRODUCT_NAME}
+            XCODE_ATTRIBUTE_PRODUCT_NAME ${PRODUCT_NAME}
+            BUNDLE true
+#            MACOSX_BUNDLE true
+            XCODE_ATTRIBUTE_MACH_O_TYPE mh_bundle
+            XCODE_ATTRIBUTE_WARNING_CFLAGS "-Wmost -Wno-four-char-constants -Wno-unknown-pragmas"
+            XCODE_ATTRIBUTE_GENERATE_PKGINFO_FILE "YES"
+            XCODE_ATTRIBUTE_DEPLOYMENT_LOCATION YES
+            XCODE_ATTRIBUTE_DSTROOT "/"
+            XCODE_ATTRIBUTE_PRODUCT_BUNDLE_IDENTIFIER ${BUNDLE_IDENTIFIER}
+            MACOSX_BUNDLE_GUI_IDENTIFIER ${BUNDLE_IDENTIFIER}
+            MACOSX_BUNDLE_BUNDLE_VERSION ${VERSION}
+            BUNDLE_EXTENSION "${OSX_EXTENSION}"
+            XCODE_ATTRIBUTE_WRAPPER_EXTENSION "${OSX_EXTENSION}"
+            XCODE_ATTRIBUTE_INSTALL_PATH "${OSX_INSTALL_PATH}"
+            XCODE_ATTRIBUTE_INFOPLIST_FILE ${VST_PLIST}
+            MACOSX_BUNDLE_INFO_PLIST ${VST_PLIST}
+            XCODE_ATTRIBUTE_INFOPLIST_PREPROCESS "YES"
+            XCODE_ATTRIBUTE_CURRENT_PROJECT_VERSION ${VERSION}
+    )
+endfunction()
+
+function(juce_add_au target sources)
+    set(OSX_EXTENSION "component")
+    set(OSX_INSTALL_PATH "$(HOME)/Library/Audio/Plug-Ins/Components/")
+    set(AU_PLIST_IN "${JUCE_CMAKE_MODULE_DIR}/FindJuceTemplates/Info-AU.plist.in")
+    set(AU_PLIST "${CMAKE_BINARY_DIR}/JuceLibraryCode/${target}_Info.plist")
+
+    configure_file("${AU_PLIST_IN}" "${AU_PLIST}" @ONLY)
+
+    add_library(${target} MODULE ${sources})
+    set_target_properties(${target} 
+        PROPERTIES 
+            OUTPUT_NAME ${PRODUCT_NAME}
+            BUNDLE true
+#            MACOSX_BUNDLE true
+            XCODE_ATTRIBUTE_MACH_O_TYPE mh_bundle
+            XCODE_ATTRIBUTE_WARNING_CFLAGS "-Wmost -Wno-four-char-constants -Wno-unknown-pragmas"
+            XCODE_ATTRIBUTE_GENERATE_PKGINFO_FILE "YES"
+            XCODE_ATTRIBUTE_DEPLOYMENT_LOCATION YES
+            XCODE_ATTRIBUTE_DSTROOT "/"
+            XCODE_ATTRIBUTE_PRODUCT_BUNDLE_IDENTIFIER ${BUNDLE_IDENTIFIER}
+            MACOSX_BUNDLE_GUI_IDENTIFIER ${BUNDLE_IDENTIFIER}
+            MACOSX_BUNDLE_BUNDLE_VERSION ${VERSION}
+            BUNDLE_EXTENSION "${OSX_EXTENSION}"
+            XCODE_ATTRIBUTE_WRAPPER_EXTENSION "${OSX_EXTENSION}"
+            XCODE_ATTRIBUTE_INSTALL_PATH "${OSX_INSTALL_PATH}"
+            XCODE_ATTRIBUTE_INFOPLIST_FILE ${AU_PLIST}
+            MACOSX_BUNDLE_INFO_PLIST ${AU_PLIST}
+            XCODE_ATTRIBUTE_INFOPLIST_PREPROCESS "YES"
+            XCODE_ATTRIBUTE_CURRENT_PROJECT_VERSION ${VERSION}
+    )
+endfunction()
+
+function(juce_generate_plugin_definitions var)
+    set(${var}
+         JucePlugin_Build_VST=${BUILD_VST}
+         JucePlugin_Build_VST3=${BUILD_VST3}           
+         JucePlugin_Build_AU=${BUILD_AU}               
+         JucePlugin_Build_AUv3=${BUILD_AUv3}            
+         JucePlugin_Build_RTAS=${BUILD_RTAS}            
+         JucePlugin_Build_AAX=${BUILD_AAX}              
+         JucePlugin_Build_Standalone=${BUILD_Standalone} 
+         JucePlugin_Enable_IAA=${ENABLE_IAA}             
+         JucePlugin_Name="${PLUGIN_NAME}"                   
+         JucePlugin_Desc="${PLUGIN_DESC}"                   
+         JucePlugin_Manufacturer="${PLUGIN_MANUFACTURER}"   
+         JucePlugin_ManufacturerWebsite="${COMPANY_WEBSITE}"
+         JucePlugin_ManufacturerEmail="${PLUGIN_MANUFACTURER_EMAIL}"      
+         JucePlugin_ManufacturerCode='${PLUGIN_MANUFACTURER_CODE}'       
+         JucePlugin_PluginCode='${PLUGIN_CODE}'
+         JucePlugin_IsSynth=${PLUGIN_IS_SYNTH}
+         JucePlugin_WantsMidiInput=${PLUGIN_WANTS_MIDI_IN}         
+         JucePlugin_ProducesMidiOutput=${PLUGIN_PRODUCES_MIDI_OUT}
+         JucePlugin_IsMidiEffect=${PLUGIN_IS_MIDI_EFFECT}
+         JucePlugin_EditorRequiresKeyboardFocus=${PLUGIN_EDITOR_REQUIRES_KEYS}
+         JucePlugin_Version=${VERSION}
+         JucePlugin_VersionCode=${VERSION_CODE}
+         JucePlugin_VersionString="${VERSION}"
+         JucePlugin_CFBundleIdentifier=${BUNDLE_IDENTIFIER}
+    )
+
+    if(${BUILD_VST})           
+        set(VST_TYPE kPlugCategEffect) # TODO: set this according to options
+        list(APPEND ${var}
+            JucePlugin_VSTUniqueID=JucePlugin_PluginCode
+            JucePlugin_VSTCategory=${VST_TYPE} 
+        )
+    endif()
+
+    if(${BUILD_AU})
+        set(AU_TYPE kAudioUnitType_MusicEffect) # TODO: set this according to options
+        list(APPEND ${var}
+             JucePlugin_AUMainType=${AU_TYPE}
+             JucePlugin_AUSubType=JucePlugin_PluginCode
+             JucePlugin_AUExportPrefix=${PLUGIN_AU_EXPORT_PREFIX}
+             JucePlugin_AUExportPrefixQuoted="${PLUGIN_AU_EXPORT_PREFIX}"
+             JucePlugin_AUManufacturerCode=JucePlugin_ManufacturerCode
+        )
+    endif()
+
+    if(${BUILD_AAX})
+        list(APPEND ${var}
+            JucePlugin_AAXIdentifier=${AAX_IDENTIFIER}
+            JucePlugin_AAXManufacturerCode=JucePlugin_ManufacturerCode
+            JucePlugin_AAXProductId=JucePlugin_PluginCode
+            JucePlugin_AAXCategory=${AAX_CATEGORY}
+            JucePlugin_AAXDisableBypass=0
+            JucePlugin_AAXDisableMultiMono=0
+        )
+    endif()
+
+    if(${BUILD_RTAS})
+        list(APPEND ${var}
+             JucePlugin_RTASCategory=ePlugInCategory_None
+             JucePlugin_RTASManufacturerCode=JucePlugin_ManufacturerCode
+             JucePlugin_RTASProductId=JucePlugin_PluginCode
+             JucePlugin_RTASDisableBypass=0
+             JucePlugin_RTASDisableMultiMono=0
+        )
+    endif()
+
+    if(${ENABLE_IAA})
+        list(APPEND ${var}
+             JucePlugin_IAAType=0x6175726d # 'aurm'
+             JucePlugin_IAASubType=JucePlugin_PluginCode
+             JucePlugin_IAAName="${COMPANY_NAME}: ${PLUGIN_NAME}"
+        )
+    endif()
+
+    foreach(definition ${${var}})
+        message("${definition}")
+    endforeach()
+
+    set(${var} ${${var}} PARENT_SCOPE)
+endfunction()
+
+function(juce_add_audio_plugin)
+
+    set(required_properties
+        PRODUCT_NAME 
+        VERSION
+        PLUGIN_NAME 
+        PLUGIN_DESC
+        PLUGIN_MANUFACTURER
+        PLUGIN_MANUFACTURER_EMAIL
+        PLUGIN_MANUFACTURER_CODE
+        PLUGIN_CODE
+        COMPANY_NAME
+        COMPANY_WEBSITE
+        BUNDLE_IDENTIFIER
+    )
+
+    set(optional_properties
+        PLUGIN_IS_SYNTH
+        PLUGIN_IS_MIDI_EFFECT
+        PLUGIN_WANTS_MIDI_IN
+        PLUGIN_PRODUCES_MIDI_OUT
+        PLUGIN_EDITOR_REQUIRES_KEYS
+        ENABLE_IAA
+    )
+
+    set(required_properties_AAX
+        AAX_IDENTIFIER
+        AAX_CATEGORY
+    )
+
+    set(required_properties_AU
+        PLUGIN_AU_EXPORT_PREFIX
+        PLUGIN_AU_VIEW_CLASS
+    )
+
+    # parse arguments
+    set(options "")
+    set(oneValueArgs 
+        PRODUCT_NAME 
+        VERSION
+        PLUGIN_NAME 
+        PLUGIN_DESC
+        PLUGIN_MANUFACTURER
+        PLUGIN_MANUFACTURER_EMAIL
+        PLUGIN_MANUFACTURER_CODE
+        PLUGIN_CODE
+        PLUGIN_IS_SYNTH
+        PLUGIN_IS_MIDI_EFFECT
+        PLUGIN_WANTS_MIDI_IN
+        PLUGIN_PRODUCES_MIDI_OUT
+        PLUGIN_EDITOR_REQUIRES_KEYS
+        COMPANY_NAME
+        COMPANY_WEBSITE
+        BUNDLE_IDENTIFIER
+        PLUGIN_AU_EXPORT_PREFIX
+        PLUGIN_AU_VIEW_CLASS
+        AAX_IDENTIFIER
+        AAX_CATEGORY
+        ENABLE_IAA
+    )
+    set(multiValueArgs FORMATS DEFINITIONS SOURCES LIBRARIES)
+    set(allArgs ${options} ${oneValueArgs} ${multiValueArgs})
+    cmake_parse_arguments(juce_add_audio_plugin 
+        "${options}" 
+        "${oneValueArgs}" 
+        "${multiValueArgs}" 
+        ${ARGN}
+    )
+
+    # validate arguments
+    foreach(property ${required_properties})
+        if(NOT DEFINED juce_add_audio_plugin_${property})
+            message("juce_add_audio_plugin: property '${property}' is required")
+            set(should_return true)
+        endif()
+    endforeach()
+
+    foreach(format ${juce_add_audio_plugins_FORMATS})
+        foreach(property ${required_properties_${format}})
+            if(NOT DEFINED juce_add_audio_plugin_${property})
+                message("juce_add_audio_plugin: property '${property}' is required for format '${format}'")
+                set(should_return true)
+            endif()
+        endforeach()
+    endforeach()
+
+    if(should_return)
+        return()
+    endif()
+
+    # import variables into local namespace
+    foreach(arg ${allArgs})
+        if(DEFINED juce_add_audio_plugin_${arg})
+            set(${arg} ${juce_add_audio_plugin_${arg}})
+        endif()
+    endforeach()
+
+    # set optional properties
+    foreach(property ${optional_properties})
+        if(DEFINED ${property})
+            if(${property})
+                set(${property} 1)
+            else()
+                set(${property} 0)
+            endif()
+        else()
+            set(${property} 0)
+        endif()
+    endforeach()
+
+    # set enabled formats
+    set(possible_formats VST AU VST3 AUv3 Standalone AAX RTAS)
+    foreach(format ${possible_formats})
+        if(${format} IN_LIST FORMATS)
+            set(BUILD_${format} 1)
+        else()
+            set(BUILD_${format} 0)
+        endif()
+    endforeach()
+
+    # TODO: set VERSION_CODE
+    set(VERSION_CODE 0x10000)
+    #set(AU_CODE)
+    #set(AU_TYPE_FOUR_CHAR)
+
+    set(PLUGIN_CODE_INT)
+    set(PLUGIN_MANUFACTURER_CODE_INT)
+
+    # generate the list of preprocessor defines
+    juce_generate_plugin_definitions(plugin_definitions)
+
+    # create common target ?
+
+    # create one target per format
+    foreach(format ${FORMATS})
+        set(target_name ${PRODUCT_NAME}${format})
+
+        if(${format} MATCHES VST)
+            juce_add_vst(${target_name} "${SOURCES}")
+        elseif(${format} MATCHES AU)
+            juce_add_au(${target_name} "${SOURCES}")
+        else()
+            message("juce_add_audio_plugins: format '${format}' not implemented")
+            return()
+        endif()
+    
+        target_compile_definitions(${target_name} PUBLIC ${plugin_definitions} ${DEFINITIONS})
+        target_link_libraries(${target_name} PUBLIC ${LIBRARIES})
+    endforeach()
+endfunction()
